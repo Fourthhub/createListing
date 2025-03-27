5543256a5dbeffc9528be477015f26124476ace9
diego <diegoechaure@gmail.com>
2025-03-27 21:24:18 +0100

    algun cambio

--- createListing/__init__.py
+++ createListing/__init__.py
@@ -23,7 +23,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Procesando la petición HTTP.')
 
     # Intenta obtener el parámetro 'id' de la query string
-    id_param = req.params.get('id')
 
     
     req_body = {}
@@ -34,12 +33,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.error("Error al obtener el body: " + str(e))
 
     # Si 'id' no viene en la query, se extrae del body
-    if not id_param:
-        id_param = req_body.get('id')
-
-    if id_param:
-        response = {"message": f"ID recibido: {id_param}"}
-        return func.HttpResponse(json.dumps(response), status_code=200, mimetype="application/json")
-    else:
-        response = {"error": "Por favor, proporciona un 'id' en la query string o en el body."}
-        return func.HttpResponse(json.dumps(response), status_code=400, mimetype="application/json")
+   
